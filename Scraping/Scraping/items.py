2d26fe712978a16bda9839474b8b98c3cd06a348
PauloSaicoski <pssarkis@inf.ufsm.br>
2020-11-18 01:46:20 -0300

    Recipe object created

--- Scraping/Scraping/items.py
+++ Scraping/Scraping/items.py
@@ -5,8 +5,31 @@
 
 import scrapy
 
-
-class ScrapingItem(scrapy.Item):
+class ExampleItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class NewItem(scrapy.Item):
+    name = scrapy.Field()
+    text = scrapy.Field()
+    tags = scrapy.Field()
+    url = scrapy.Field()
+    project = scrapy.Field()
+    spider = scrapy.Field()
+
+class Recipe(scrapy.Item):
+    name = scrapy.Field()
+    prepTime = scrapy.Field()
+    portions = scrapy.Field()
+    likes = scrapy.Field()
+    comments = scrapy.Field()
+
+    ingredients = scrapy.Field()
+
+    url = scrapy.Field()
+    project = scrapy.Field()
+    spider = scrapy.Field()
+
+class Test(scrapy.Item):
+    names = scrapy.Field()
